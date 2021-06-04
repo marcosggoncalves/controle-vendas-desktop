9cc832f84675aec23df521d21aeeb48d7a3d492d
Marcos Lopes <marcoslopesg7@gmail.com>
2021-06-04 17:32:28 -0300

    Adicionando drivers necessários

--- src/br/com/projeto/jdbc/ConnectionFactory.java
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -15,7 +15,7 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
     public Connection getConnection(){
         try{
-            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/bdvendas","root","");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bdvendas","root","");
         }catch(Exception error){
              throw new RuntimeException(error);
         }
